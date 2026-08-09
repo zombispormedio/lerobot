83927a889395e73fa2810c18fbf702ee6c1e1fd6
Xavier Serrano <zombispormedio007@gmail.com>
2026-08-09 14:07:02 +0200

    refactor(config): comment out unused camera configurations in xlerobot

--- src/lerobot/robots/xlerobot/config_xlerobot.py
+++ src/lerobot/robots/xlerobot/config_xlerobot.py
@@ -23,17 +23,17 @@ from ..config import RobotConfig
 
 def xlerobot_cameras_config() -> dict[str, CameraConfig]:
     return {
-        "left_wrist": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
-        ),
+        # "left_wrist": OpenCVCameraConfig(
+        #     index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),
 
-        "right_wrist": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
-        ),  
+        # "right_wrist": OpenCVCameraConfig(
+        #     index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),  
 
-        "head(RGDB)": OpenCVCameraConfig(
-            index_or_path="/dev/video4", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
-        ),                     
+        # "head(RGDB)": OpenCVCameraConfig(
+        #     index_or_path="/dev/video4", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),                     
         
         # "head": RealSenseCameraConfig(
         #     serial_number_or_name="125322060037",  # Replace with camera SN
