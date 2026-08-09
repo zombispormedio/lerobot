37b03aeaee2a441d30cf30846e46545e917ee5bc
Xavier Serrano <zombispormedio007@gmail.com>
2026-08-09 20:03:53 +0200

    fix(imports): include XLerobotClientConfig in xlerobot module imports

--- src/lerobot/robots/xlerobot/__init__.py
+++ src/lerobot/robots/xlerobot/__init__.py
@@ -1,4 +1,4 @@
 from .config_xlerobot import XLerobotConfig
 from .xlerobot import XLerobot
-from .xlerobot_client import XLerobotClient
+from .xlerobot_client import XLerobotClient, XLerobotClientConfig
 from .xlerobot_host import XLerobotHost, XLerobotHostConfig
